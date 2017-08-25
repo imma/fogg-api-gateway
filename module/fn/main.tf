019985cee4123a1a9d241b8b3b3972ff09f8879d
Defn Nghiem <iam@defn.sh>
2017-08-25 04:20:48 +0000

    api gateway fn, resource

---
+++ module/fn/main.tf
@@ -0,0 +1,27 @@
+variable "function_name" {}
+variable "unique_prefix" {}
+variable "source_arn" {}
+variable "role" {}
+variable "deployment_zip" {}
+
+resource "aws_lambda_function" "fn" {
+  filename         = "${var.deployment_zip}"
+  function_name    = "${var.function_name}"
+  role             = "${var.role}"
+  handler          = "app.app"
+  runtime          = "python3.6"
+  source_code_hash = "${base64sha256(file("${var.deployment_zip}"))}"
+  publish          = true
+}
+
+resource "aws_lambda_permission" "fn" {
+  statement_id  = "${var.unique_prefix}-${var.function_name}"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  function_name = "${aws_lambda_function.fn.function_name}"
+  source_arn    = "${var.source_arn}"
+}
+
+output "invoke_arn" {
+  value = "${aws_lambda_function.fn.invoke_arn}"
+}
---
+++ module/resource/main.tf
@@ -0,0 +1,37 @@
+variable "api_name" {}
+variable "rest_api_id" {}
+variable "resource_id" {}
+
+variable "http_method" {
+  default = "POST"
+}
+
+variable "invoke_arn" {}
+
+resource "aws_api_gateway_resource" "fn" {
+  rest_api_id = "${var.rest_api_id}"
+  parent_id   = "${var.resource_id}"
+  path_part   = "${var.api_name}"
+}
+
+resource "aws_api_gateway_method" "fn" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${aws_api_gateway_resource.fn.id}"
+  http_method   = "${var.http_method}"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "fn" {
+  rest_api_id             = "${var.rest_api_id}"
+  resource_id             = "${aws_api_gateway_resource.fn.id}"
+  http_method             = "${aws_api_gateway_method.fn.http_method}"
+  uri                     = "${var.invoke_arn}"
+  integration_http_method = "${aws_api_gateway_method.fn.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  content_handling        = "CONVERT_TO_TEXT"
+}
+
+output "resource" {
+  value = "${aws_api_gateway_resource.fn.id}"
+}
