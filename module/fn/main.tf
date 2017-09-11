51a70c7bffd8b40d986cae0f98a579717818cd20
Defn Nghiem <iam@defn.sh>
2017-09-10 20:06:00 -0700

    let the caller create the lambda function

--- module/fn/main.tf
+++ module/fn/main.tf
@@ -3,30 +3,44 @@ variable "unique_prefix" {}
 variable "source_arn" {}
 variable "role" {}
 variable "deployment_zip" {}
+variable "fn_arn" {}
 
-resource "aws_lambda_function" "fn" {
-  filename         = "${var.deployment_zip}"
-  function_name    = "${var.function_name}"
-  role             = "${var.role}"
-  handler          = "app.app"
-  runtime          = "python3.6"
-  source_code_hash = "${base64sha256(file("${var.deployment_zip}"))}"
-  publish          = true
+variable "fn_dev" {
+  default = "$LATEST"
+}
+
+variable "fn_prod" {
+  default = "$LATEST"
 }
 
 resource "aws_lambda_alias" "prod" {
   name             = "prod"
-  function_name    = "${aws_lambda_function.fn.function_name}"
-  function_version = "$LATEST"
+  function_name    = "${var.fn_arn}"
+  function_version = "${var.fn_prod}"
 }
 
-resource "aws_lambda_permission" "fn" {
+resource "aws_lambda_alias" "dev" {
+  name             = "dev"
+  function_name    = "${var.fn_arn}"
+  function_version = "${var.fn_dev}"
+}
+
+resource "aws_lambda_permission" "prod" {
   statement_id  = "${var.unique_prefix}-${var.function_name}"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
   function_name = "${aws_lambda_alias.fn.name}"
   source_arn    = "${var.source_arn}"
   qualifier     = "${aws_lambda_alias.prod.name}"
+}
+
+resource "aws_lambda_permission" "dev" {
+  statement_id  = "${var.unique_prefix}-${var.function_name}"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  function_name = "${aws_lambda_alias.fn.name}"
+  source_arn    = "${var.source_arn}"
+  qualifier     = "${aws_lambda_alias.dev.name}"
 }
 
 output "invoke_arn" {
