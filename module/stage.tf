5442e6c53a38509892d47f4af89ebfc4e8f92d4f
Defn Nghiem <iam@defn.sh>
2017-09-21 17:45:58 -0700

    move stage here

---
+++ module/stage.tf
@@ -0,0 +1,31 @@
+variable "rest_api_id" {}
+variable "stage_name" {}
+variable "domain_name" {}
+
+resource "aws_api_gateway_deployment" "stage" {
+  rest_api_id = "${var.rest_api_id}"
+  stage_name  = "${var.stage_name}"
+
+  variables {
+    alias = "${var.stage_name}"
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "stage" {
+  api_id      = "${var.rest_api_id}"
+  stage_name  = "${var.stage_name}"
+  domain_name = "${var.domain_name}"
+  base_path   = "${var.stage_name}"
+}
+
+resource "aws_api_gateway_method_settings" "stage" {
+  rest_api_id = "${var.rest_api_id}"
+  stage_name  = "${var.stage_name}"
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled    = true
+    logging_level      = "INFO"
+    data_trace_enabled = true
+  }
+}
