15b66cbcbc8ce8facc777c95e6f5656b1e20cfda
Defn Nghiem <iam@defn.sh>
2017-10-12 14:35:45 -0700

    wildcards

--- module/resource/main.tf
+++ module/resource/main.tf
@@ -3,7 +3,7 @@ variable "rest_api_id" {}
 variable "resource_id" {}
 
 variable "http_method" {
-  default = "POST"
+  default = "GET"
 }
 
 variable "invoke_arn" {}
@@ -21,12 +21,36 @@ resource "aws_api_gateway_method" "fn" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_resource" "fn_catch_all" {
+  rest_api_id = "${var.rest_api_id}"
+  parent_id   = "${aws_api_gateway_resource.fn.id}"
+  path_part   = "{catch_all+}"
+}
+
+resource "aws_api_gateway_method" "fn_catch_all" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${aws_api_gateway_resource.fn_catch_all.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "fn" {
   rest_api_id             = "${var.rest_api_id}"
   resource_id             = "${aws_api_gateway_resource.fn.id}"
-  http_method             = "${aws_api_gateway_method.fn.http_method}"
   uri                     = "${replace(var.invoke_arn,"/invocations",":$${stageVariables.alias}/invocations")}"
+  http_method             = "${aws_api_gateway_method.fn.http_method}"
   integration_http_method = "${aws_api_gateway_method.fn.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  content_handling        = "CONVERT_TO_TEXT"
+}
+
+resource "aws_api_gateway_integration" "fn_catch_all" {
+  rest_api_id             = "${var.rest_api_id}"
+  resource_id             = "${aws_api_gateway_resource.fn_catch_all.id}"
+  uri                     = "${replace(var.invoke_arn,"/invocations",":$${stageVariables.alias}/invocations")}"
+  http_method             = "${aws_api_gateway_method.fn_catch_all.http_method}"
+  integration_http_method = "${aws_api_gateway_method.fn_catch_all.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"
