816892cd3ce618e3ed6ec566b31084a63187deb3
Defn Nghiem <iam@defn.sh>
2017-10-16 12:49:45 -0700

    force httpd method

--- module/resource/main.tf
+++ module/resource/main.tf
@@ -1,11 +1,7 @@
 variable "api_name" {}
 variable "rest_api_id" {}
 variable "resource_id" {}
-
-variable "http_method" {
-  default = "GET"
-}
-
+variable "http_method" {}
 variable "invoke_arn" {}
 
 resource "aws_api_gateway_resource" "fn" {
@@ -39,7 +35,6 @@ resource "aws_api_gateway_integration" "fn" {
   resource_id             = "${aws_api_gateway_resource.fn.id}"
   uri                     = "${replace(var.invoke_arn,"/invocations",":$${stageVariables.alias}/invocations")}"
   http_method             = "${aws_api_gateway_method.fn.http_method}"
-  integration_http_method = "${aws_api_gateway_method.fn.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"
